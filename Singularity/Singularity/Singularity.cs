4b1a7651bcd9f7acb87071c5c0186189ec792ec7
lghassen <11978271+LGhassen@users.noreply.github.com>
2026-04-30 13:15:04 +0200

    Add a second gui modifier key and default to ctrl+alt+S to avoid triggering UI randomly when playing, fixes #9, reformat tabs from old project to spaces

--- Singularity/Singularity/Singularity.cs
+++ Singularity/Singularity/Singularity.cs
@@ -5,259 +5,263 @@ using System.IO;
 using System.Reflection;
 using UnityEngine;
 
-[assembly: AssemblyVersion("0.992.*")]
+[assembly: AssemblyVersion("0.993.*")]
 namespace Singularity
 {
-	[KSPAddon(KSPAddon.Startup.AllGameScenes, false)]
-	public class Singularity : MonoBehaviour
-	{
-		private static Singularity instance;
-		public static Singularity Instance {get {return instance;}}
-		
-		private static Dictionary<string, Shader> LoadedShadersDictionary = new Dictionary<string, Shader>();				
-		public static Dictionary<string, Shader> LoadedShaders {get{return LoadedShadersDictionary;}}
-		
-		private string path,gameDataPath;		
-		public string GameDataPath {get{return gameDataPath;}}
+    [KSPAddon(KSPAddon.Startup.AllGameScenes, false)]
+    public class Singularity : MonoBehaviour
+    {
+        private static Singularity instance;
+        public static Singularity Instance { get { return instance; } }
 
-		public Cubemap galaxyCubemap;
-		public MaterialPropertyBlock galaxyCubeControlMPB;
+        private static Dictionary<string, Shader> LoadedShadersDictionary = new Dictionary<string, Shader>();
+        public static Dictionary<string, Shader> LoadedShaders { get { return LoadedShadersDictionary; } }
 
-		public List<SingularityObject> loadedObjects = new List<SingularityObject>();
-		
-		public RenderTexture screenBufferFlip, screenBufferFlop, stackingDepthBuffer;
+        private string path, gameDataPath;
+        public string GameDataPath { get { return gameDataPath; } }
 
-		public ScaledSceneBufferRenderer scaledSceneBufferRenderer;
+        public Cubemap galaxyCubemap;
+        public MaterialPropertyBlock galaxyCubeControlMPB;
 
-		double initialUniversalTime;
+        public List<SingularityObject> loadedObjects = new List<SingularityObject>();
 
-		[Persistent] public int galaxyCubemapResolution = 2048;
-		[Persistent] public int objectCubemapResolution = 2048;
-		[Persistent] public bool lensingStacking = true;
+        public RenderTexture screenBufferFlip, screenBufferFlop, stackingDepthBuffer;
 
-		public Singularity ()
-		{
-			if (instance == null)
-			{
-				instance = this;
-				Utils.LogInfo("Instance created");
-			}
-			else
-			{
-				//destroy any duplicate instances that may be created by a duplicate install
-				Utils.LogError("Destroying duplicate instance, check your install for duplicate mod folders");
-				UnityEngine.Object.Destroy (this);
-			}
-		}
+        public ScaledSceneBufferRenderer scaledSceneBufferRenderer;
 
-		void Awake()
-		{
-			string codeBase = Assembly.GetExecutingAssembly ().CodeBase;
-			UriBuilder uri = new UriBuilder (codeBase);
-			
-			path = Uri.UnescapeDataString (uri.Path);
-			path = Path.GetDirectoryName (path);
-			
-			gameDataPath = KSPUtil.ApplicationRootPath + "GameData/";	
-			
-			LoadedShadersDictionary = Utils.LoadAssetBundle (path);
-			StartCoroutine (DelayedInit ());
-		}
+        double initialUniversalTime;
 
-		// Delay for the galaxy cubemap to be set correctly
-		IEnumerator DelayedInit()
-		{
-			for (int i=0; i<5; i++)
-			{
-				yield return new WaitForFixedUpdate ();
-			}
-			
-			Init();
-		}
+        [Persistent] public int galaxyCubemapResolution = 2048;
+        [Persistent] public int objectCubemapResolution = 2048;
+        [Persistent] public bool lensingStacking = true;
 
-		void Init()
-		{
-			LoadGeneralSettings ();
+        [Persistent] public KeyCode guiModifierKey1 = KeyCode.LeftControl;
+        [Persistent] public KeyCode guiModifierKey2 = KeyCode.LeftAlt;
+        [Persistent] public KeyCode guiKey = KeyCode.S;
 
-			SetupCubemap ();
+        public Singularity()
+        {
+            if (instance == null)
+            {
+                instance = this;
+                Utils.LogInfo("Instance created");
+            }
+            else
+            {
+                //destroy any duplicate instances that may be created by a duplicate install
+                Utils.LogError("Destroying duplicate instance, check your install for duplicate mod folders");
+                UnityEngine.Object.Destroy(this);
+            }
+        }
 
-			screenBufferFlip = new RenderTexture(Screen.width, Screen.height, 24, ScaledCamera.Instance.cam.allowHDR ? RenderTextureFormat.ARGBHalf : RenderTextureFormat.ARGB32, 9);
-			screenBufferFlip.filterMode = FilterMode.Bilinear;
-			screenBufferFlip.name = "flipBuffer";
-			screenBufferFlip.Create ();
+        void Awake()
+        {
+            string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+            UriBuilder uri = new UriBuilder(codeBase);
 
-			if (lensingStacking)
-			{
-				screenBufferFlop = new RenderTexture (Screen.width, Screen.height, 0, ScaledCamera.Instance.cam.allowHDR ? RenderTextureFormat.ARGBHalf : RenderTextureFormat.ARGB32, 9); //no depth on purpose, bind depth from flip buffer when rendering
-				screenBufferFlop.filterMode = FilterMode.Bilinear;
-				screenBufferFlop.Create ();
-				screenBufferFlop.name = "flopBuffer";
+            path = Uri.UnescapeDataString(uri.Path);
+            path = Path.GetDirectoryName(path);
 
-				stackingDepthBuffer = new RenderTexture (Screen.width, Screen.height, 24, RenderTextureFormat.Depth);
-				stackingDepthBuffer.anisoLevel = 1;
-				stackingDepthBuffer.antiAliasing = 1;
-				stackingDepthBuffer.volumeDepth = 0;
-				stackingDepthBuffer.useMipMap = false;
-				stackingDepthBuffer.autoGenerateMips = false;
-				stackingDepthBuffer.filterMode = FilterMode.Point;
-				stackingDepthBuffer.depth = 24;
-				stackingDepthBuffer.Create ();
-				stackingDepthBuffer.name="stackingDepthBuffer";
+            gameDataPath = KSPUtil.ApplicationRootPath + "GameData/";
 
-				StackedLensingRenderer.Create();
-			}
-			
-			scaledSceneBufferRenderer = gameObject.AddComponent<ScaledSceneBufferRenderer>();
+            LoadedShadersDictionary = Utils.LoadAssetBundle(path);
+            StartCoroutine(DelayedInit());
+        }
 
-			scaledSceneBufferRenderer.Init ();
+        // Delay for the galaxy cubemap to be set correctly
+        IEnumerator DelayedInit()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                yield return new WaitForFixedUpdate();
+            }
 
-			initialUniversalTime = Planetarium.GetUniversalTime ();
+            Init();
+        }
 
-			LoadSingularities ();
-		}
+        void Init()
+        {
+            LoadGeneralSettings();
 
-		void SetupCubemap()
-		{
-			try
-			{
-				galaxyCubeControlMPB = typeof(GalaxyCubeControl).GetField ("mpb", Utils.reflectionFlags).GetValue (GalaxyCubeControl.Instance) as MaterialPropertyBlock;
-				UnityEngine.Renderer[] cubeRenderers = typeof(GalaxyCubeControl).GetField ("cubeRenderers", Utils.reflectionFlags).GetValue (GalaxyCubeControl.Instance) as UnityEngine.Renderer[];				
-				Component galaxyCubeControlComponent = (Component) GalaxyCubeControl.Instance;
-				
-				if (!ReferenceEquals (galaxyCubeControlMPB, null) && !ReferenceEquals(cubeRenderers,null))
-				{
-					// Disable cubemap dimming before we capture it
-					galaxyCubeControlMPB.SetColor(PropertyIDs._Color, Color.white);					
-					for (int i = 0; i < cubeRenderers.Length; i++)
-					{
-						cubeRenderers[i].SetPropertyBlock(galaxyCubeControlMPB);	
-					}					
-					// De-rotate galaxy cubemap before we capture it, later adjust in shader for additional planetarium rotations
-					GalaxyCubeControl.Instance.transform.rotation = GalaxyCubeControl.Instance.initRot;
-				}
-			}
-			catch (Exception E)
-			{
-				Utils.LogError("Couldn't setup galaxy cubeMap correctly, Exception thrown: "+E.ToString());
-			}
-			
-			galaxyCubemap = new Cubemap (galaxyCubemapResolution, TextureFormat.ARGB32, 9);
-			ScaledCamera.Instance.galaxyCamera.RenderToCubemap (galaxyCubemap);	//probably we should restore cubemap dimming here, seems it doesn't restore itself in the KSC screen?
-			Utils.LogInfo ("GalaxyCubemap initialized");
-		}
+            SetupCubemap();
 
-		void LoadGeneralSettings()
-		{
-			UrlDir.UrlConfig[] configs = GameDatabase.Instance.GetConfigs ("Singularity");
-			foreach (UrlDir.UrlConfig _url in configs)
-			{
-				ConfigNode[] configNodeArray = _url.config.GetNodes ("Singularity_config");
-				if (configNodeArray.Length > 0)
-				{
-					ConfigNode.LoadObjectFromConfig (this, configNodeArray [0]);			
-					Utils.LogInfo ("Config loaded, resolutions: galaxyCubemapResolution=" + galaxyCubemapResolution.ToString () + ", objectCubemapResolution=" + objectCubemapResolution.ToString ());
-					return;
-				}
-			}
-		}
+            screenBufferFlip = new RenderTexture(Screen.width, Screen.height, 24, ScaledCamera.Instance.cam.allowHDR ? RenderTextureFormat.ARGBHalf : RenderTextureFormat.ARGB32, 9);
+            screenBufferFlip.filterMode = FilterMode.Bilinear;
+            screenBufferFlip.name = "flipBuffer";
+            screenBufferFlip.Create();
 
-		void LoadSingularities()
-		{
-			UrlDir.UrlConfig[] configs = GameDatabase.Instance.GetConfigs ("Singularity");
+            if (lensingStacking)
+            {
+                screenBufferFlop = new RenderTexture(Screen.width, Screen.height, 0, ScaledCamera.Instance.cam.allowHDR ? RenderTextureFormat.ARGBHalf : RenderTextureFormat.ARGB32, 9); //no depth on purpose, bind depth from flip buffer when rendering
+                screenBufferFlop.filterMode = FilterMode.Bilinear;
+                screenBufferFlop.Create();
+                screenBufferFlop.name = "flopBuffer";
 
-			foreach (UrlDir.UrlConfig _url in configs)
-			{
-				ConfigNode[] configNodeArray = _url.config.GetNodes("Singularity_object");
+                stackingDepthBuffer = new RenderTexture(Screen.width, Screen.height, 24, RenderTextureFormat.Depth);
+                stackingDepthBuffer.anisoLevel = 1;
+                stackingDepthBuffer.antiAliasing = 1;
+                stackingDepthBuffer.volumeDepth = 0;
+                stackingDepthBuffer.useMipMap = false;
+                stackingDepthBuffer.autoGenerateMips = false;
+                stackingDepthBuffer.filterMode = FilterMode.Point;
+                stackingDepthBuffer.depth = 24;
+                stackingDepthBuffer.Create();
+                stackingDepthBuffer.name = "stackingDepthBuffer";
 
-				foreach(ConfigNode _cn in configNodeArray)
-				{
-					AddSingularityObject (_cn);
-				}
-			}
-		}
+                StackedLensingRenderer.Create();
+            }
 
-		void AddSingularityObject (ConfigNode _cn)
-		{
-			if (_cn.HasValue ("name") && _cn.HasValue ("gravity"))
-			{
-				Transform scaledBodyTransform = ScaledSpace.Instance.transform.FindChild (_cn.GetValue ("name"));
-				if (!ReferenceEquals (scaledBodyTransform, null))
-				{
-					try
-					{
-						SingularityObject singularityObject = scaledBodyTransform.gameObject.AddComponent<SingularityObject> ();
-						loadedObjects.Add(singularityObject);
-						singularityObject.Init (_cn);
-					}
-					catch (Exception e)
-					{
-						Utils.LogError ("Couldn't add singularity object to " + _cn.GetValue ("name") + ", Exception thrown: " + e.ToString ());
-					}
-				}
-				else
-				{
-					Utils.LogError ("Unable to find " + _cn.GetValue ("name") + ", skipping ...");
-				}
-			}
-		}
+            scaledSceneBufferRenderer = gameObject.AddComponent<ScaledSceneBufferRenderer>();
 
-		void OnDestroy()
-		{
-			foreach (SingularityObject singularityObject in loadedObjects)
-			{
-				singularityObject.OnDestroy();
-				UnityEngine.Object.Destroy(singularityObject);
-			}
+            scaledSceneBufferRenderer.Init();
 
-			screenBufferFlip.Release ();
-			if (screenBufferFlop.IsCreated())
-				screenBufferFlop.Release ();
+            initialUniversalTime = Planetarium.GetUniversalTime();
 
-			if (!ReferenceEquals(scaledSceneBufferRenderer,null))
-			{
-				scaledSceneBufferRenderer.Cleanup();
-			}
-		}
+            LoadSingularities();
+        }
 
-		public void DisableSingularitiesForSceneBuffer()
-		{
-			foreach (SingularityObject singularityObject in loadedObjects)
-			{
-				singularityObject.DisableForSceneOrCubemap();
-			}
-		}
+        void SetupCubemap()
+        {
+            try
+            {
+                galaxyCubeControlMPB = typeof(GalaxyCubeControl).GetField("mpb", Utils.reflectionFlags).GetValue(GalaxyCubeControl.Instance) as MaterialPropertyBlock;
+                UnityEngine.Renderer[] cubeRenderers = typeof(GalaxyCubeControl).GetField("cubeRenderers", Utils.reflectionFlags).GetValue(GalaxyCubeControl.Instance) as UnityEngine.Renderer[];
+                Component galaxyCubeControlComponent = (Component)GalaxyCubeControl.Instance;
 
-		public void ReEnableSingularities()
-		{
-			foreach (SingularityObject singularityObject in loadedObjects)
-			{
-				singularityObject.ReEnable();
-			}
-		}
-		
-		public void SwitchSingularitiesToNormalMode()
-		{
-			foreach (SingularityObject singularityObject in loadedObjects)
-			{
-				singularityObject.SwitchToNormalMode();
-			}
-		}
-		
-		public void SwitchSingularitiesToCopyMode()
-		{
-			foreach (SingularityObject singularityObject in loadedObjects)
-			{
-				singularityObject.SwitchToCopyMode();
-			}
-		}
+                if (!ReferenceEquals(galaxyCubeControlMPB, null) && !ReferenceEquals(cubeRenderers, null))
+                {
+                    // Disable cubemap dimming before we capture it
+                    galaxyCubeControlMPB.SetColor(PropertyIDs._Color, Color.white);
+                    for (int i = 0; i < cubeRenderers.Length; i++)
+                    {
+                        cubeRenderers[i].SetPropertyBlock(galaxyCubeControlMPB);
+                    }
+                    // De-rotate galaxy cubemap before we capture it, later adjust in shader for additional planetarium rotations
+                    GalaxyCubeControl.Instance.transform.rotation = GalaxyCubeControl.Instance.initRot;
+                }
+            }
+            catch (Exception E)
+            {
+                Utils.LogError("Couldn't setup galaxy cubeMap correctly, Exception thrown: " + E.ToString());
+            }
 
-		// universal time is a large double, if we pass it to the shader directly as a float we lose enough precision that things look like they run at lower fps
-		// pass the offset from an initial value as float
-		public float getTime()
-		{
-			return (float)(Planetarium.GetUniversalTime () - initialUniversalTime);
-		}
-	}
+            galaxyCubemap = new Cubemap(galaxyCubemapResolution, TextureFormat.ARGB32, 9);
+            ScaledCamera.Instance.galaxyCamera.RenderToCubemap(galaxyCubemap);  //probably we should restore cubemap dimming here, seems it doesn't restore itself in the KSC screen?
+            Utils.LogInfo("GalaxyCubemap initialized");
+        }
+
+        void LoadGeneralSettings()
+        {
+            UrlDir.UrlConfig[] configs = GameDatabase.Instance.GetConfigs("Singularity");
+            foreach (UrlDir.UrlConfig _url in configs)
+            {
+                ConfigNode[] configNodeArray = _url.config.GetNodes("Singularity_config");
+                if (configNodeArray.Length > 0)
+                {
+                    ConfigNode.LoadObjectFromConfig(this, configNodeArray[0]);
+                    Utils.LogInfo("Config loaded, resolutions: galaxyCubemapResolution=" + galaxyCubemapResolution.ToString() + ", objectCubemapResolution=" + objectCubemapResolution.ToString());
+                    return;
+                }
+            }
+        }
+
+        void LoadSingularities()
+        {
+            UrlDir.UrlConfig[] configs = GameDatabase.Instance.GetConfigs("Singularity");
+
+            foreach (UrlDir.UrlConfig _url in configs)
+            {
+                ConfigNode[] configNodeArray = _url.config.GetNodes("Singularity_object");
+
+                foreach (ConfigNode _cn in configNodeArray)
+                {
+                    AddSingularityObject(_cn);
+                }
+            }
+        }
+
+        void AddSingularityObject(ConfigNode _cn)
+        {
+            if (_cn.HasValue("name") && _cn.HasValue("gravity"))
+            {
+                Transform scaledBodyTransform = ScaledSpace.Instance.transform.FindChild(_cn.GetValue("name"));
+                if (!ReferenceEquals(scaledBodyTransform, null))
+                {
+                    try
+                    {
+                        SingularityObject singularityObject = scaledBodyTransform.gameObject.AddComponent<SingularityObject>();
+                        loadedObjects.Add(singularityObject);
+                        singularityObject.Init(_cn);
+                    }
+                    catch (Exception e)
+                    {
+                        Utils.LogError("Couldn't add singularity object to " + _cn.GetValue("name") + ", Exception thrown: " + e.ToString());
+                    }
+                }
+                else
+                {
+                    Utils.LogError("Unable to find " + _cn.GetValue("name") + ", skipping ...");
+                }
+            }
+        }
+
+        void OnDestroy()
+        {
+            foreach (SingularityObject singularityObject in loadedObjects)
+            {
+                singularityObject.OnDestroy();
+                UnityEngine.Object.Destroy(singularityObject);
+            }
+
+            screenBufferFlip.Release();
+            if (screenBufferFlop.IsCreated())
+                screenBufferFlop.Release();
+
+            if (!ReferenceEquals(scaledSceneBufferRenderer, null))
+            {
+                scaledSceneBufferRenderer.Cleanup();
+            }
+        }
+
+        public void DisableSingularitiesForSceneBuffer()
+        {
+            foreach (SingularityObject singularityObject in loadedObjects)
+            {
+                singularityObject.DisableForSceneOrCubemap();
+            }
+        }
+
+        public void ReEnableSingularities()
+        {
+            foreach (SingularityObject singularityObject in loadedObjects)
+            {
+                singularityObject.ReEnable();
+            }
+        }
+
+        public void SwitchSingularitiesToNormalMode()
+        {
+            foreach (SingularityObject singularityObject in loadedObjects)
+            {
+                singularityObject.SwitchToNormalMode();
+            }
+        }
+
+        public void SwitchSingularitiesToCopyMode()
+        {
+            foreach (SingularityObject singularityObject in loadedObjects)
+            {
+                singularityObject.SwitchToCopyMode();
+            }
+        }
+
+        // universal time is a large double, if we pass it to the shader directly as a float we lose enough precision that things look like they run at lower fps
+        // pass the offset from an initial value as float
+        public float getTime()
+        {
+            return (float)(Planetarium.GetUniversalTime() - initialUniversalTime);
+        }
+    }
 }
 
 
--- Singularity/Singularity/SingularityUI.cs
+++ Singularity/Singularity/SingularityUI.cs
@@ -12,129 +12,137 @@ using UnityEngine;
 
 namespace Singularity
 {
-	[KSPAddon(KSPAddon.Startup.AllGameScenes, false)]
-	public class SingularityUI: MonoBehaviour
-	{
-		String[] selStrings;
-		
-		bool uiVisible = false;
-		Rect windowRect = new Rect (0, 0, 400, 50);
-		int windowId;
-		int selGridInt = 0;
-		bool editing = false;
-		string nodeText = "";
-		private Vector2 scrollPosition = new Vector2();
-		
-		public SingularityUI ()
-		{
+    [KSPAddon(KSPAddon.Startup.AllGameScenes, false)]
+    public class SingularityUI : MonoBehaviour
+    {
+        String[] selStrings;
 
-		}
+        bool uiVisible = false;
+        Rect windowRect = new Rect(0, 0, 400, 50);
+        int windowId;
+        int selGridInt = 0;
+        bool editing = false;
+        string nodeText = "";
+        private Vector2 scrollPosition = new Vector2();
 
-		public void Awake()
-		{
-			windowId = UnityEngine.Random.Range(int.MinValue, int.MaxValue);
+        public SingularityUI()
+        {
 
-			// Wait for the main plugin to be initialized
-			StartCoroutine (DelayedInit ());
-		}
-		
-		IEnumerator DelayedInit()
-		{
-			for (int i=0; i<6; i++)
-			{
-				yield return new WaitForFixedUpdate ();
-			}
-			
-			Init();
-		}
+        }
 
-		void Init()
-		{
-			selStrings = new string[Singularity.Instance.loadedObjects.Count];
-			
-			for (int i=0; i<Singularity.Instance.loadedObjects.Count; i++)
-			{
-				selStrings[i]=Singularity.Instance.loadedObjects[i].name;
-			}
-		}
-		
-		void OnGUI ()
-		{
-			if (uiVisible)
-			{
-				windowRect = GUILayout.Window (windowId, windowRect, DrawWindow,$"Singularity {Assembly.GetExecutingAssembly().GetName().Version.Major}.{Assembly.GetExecutingAssembly().GetName().Version.Minor}");
-				
-				//prevent window from going offscreen
-				windowRect.x = Mathf.Clamp(windowRect.x,0,Screen.width-windowRect.width);
-				windowRect.y = Mathf.Clamp(windowRect.y,0,Screen.height-windowRect.height);
-			}
-		}
-		
-		internal void Update()
-		{
-			if (GameSettings.MODIFIER_KEY.GetKey () && Input.GetKeyDown (KeyCode.S))
-			{
-				uiVisible = !uiVisible;
-			}
-		}
-		
-		public void DrawWindow (int windowId)
-		{
-			if (Singularity.Instance.loadedObjects.Count != 0)
-			{
-				GUILayout.BeginVertical ();
-				GUILayout.Label ("Choose singularity to edit");
-				selGridInt = GUILayout.SelectionGrid (selGridInt, selStrings, 1);
-				if (GUILayout.Button ("Edit Selected"))
-				{
-					if (!ReferenceEquals (Singularity.Instance.loadedObjects[selGridInt], null))
-					{
-						nodeText = string.Copy (Utils.WriteRootNode(ConfigNode.CreateConfigFromObject(Singularity.Instance.loadedObjects[selGridInt])));
-						editing = true;
-					}
-				}
-				GUILayout.EndVertical ();
-				
-				if (editing)
-				{
-					scrollPosition = GUILayout.BeginScrollView (scrollPosition, false, true, GUILayout.MinHeight (300));				
-					nodeText = GUILayout.TextArea (nodeText, GUILayout.ExpandWidth (true), GUILayout.ExpandHeight (true));
-					GUILayout.EndScrollView ();
-					
-					GUILayout.BeginHorizontal ();
-					
-					if (GUILayout.Button ("Reimport"))
-					{
-						nodeText = string.Copy (Utils.WriteRootNode(ConfigNode.CreateConfigFromObject(Singularity.Instance.loadedObjects[selGridInt])));
-					}
-					
-					if (GUILayout.Button ("Apply"))
-					{
-						ConfigNode node = ConfigNode.Parse (nodeText);
-						Singularity.Instance.loadedObjects[selGridInt].ApplyFromUI(node);
-					}
+        public void Awake()
+        {
+            windowId = UnityEngine.Random.Range(int.MinValue, int.MaxValue);
 
-					if (GUILayout.Button ("Copy to clipboard"))
-					{
-						GUIUtility.systemCopyBuffer = nodeText;
-					}
+            // Wait for the main plugin to be initialized
+            StartCoroutine(DelayedInit());
+        }
 
-					if (GUILayout.Button ("Print to Log"))
-					{
-						Utils.LogInfo("Config print to log:\r\n"+nodeText);
-					}
-					
-					GUILayout.EndHorizontal ();
-				}
-			}
-			else
-			{
-				GUILayout.Label ("No Singularity objects loaded");
-			}
-			
-			GUI.DragWindow();
-		}
-	}
+        IEnumerator DelayedInit()
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                yield return new WaitForFixedUpdate();
+            }
+
+            Init();
+        }
+
+        void Init()
+        {
+            selStrings = new string[Singularity.Instance.loadedObjects.Count];
+
+            for (int i = 0; i < Singularity.Instance.loadedObjects.Count; i++)
+            {
+                selStrings[i] = Singularity.Instance.loadedObjects[i].name;
+            }
+        }
+
+        void OnGUI()
+        {
+            if (uiVisible)
+            {
+                windowRect = GUILayout.Window(windowId, windowRect, DrawWindow, $"Singularity {Assembly.GetExecutingAssembly().GetName().Version.Major}.{Assembly.GetExecutingAssembly().GetName().Version.Minor}");
+
+                //prevent window from going offscreen
+                windowRect.x = Mathf.Clamp(windowRect.x, 0, Screen.width - windowRect.width);
+                windowRect.y = Mathf.Clamp(windowRect.y, 0, Screen.height - windowRect.height);
+            }
+        }
+
+        bool guiKeyWasPressed = false;
+
+        internal void Update()
+        {
+            bool guiKeyPressed = Input.GetKey(Singularity.Instance.guiModifierKey1)
+                && Input.GetKey(Singularity.Instance.guiModifierKey2)
+                && Input.GetKey(Singularity.Instance.guiKey);
+
+            if (guiKeyPressed && !guiKeyWasPressed)
+            {
+                uiVisible = !uiVisible;
+            }
+
+            guiKeyWasPressed = guiKeyPressed;
+        }
+
+        public void DrawWindow(int windowId)
+        {
+            if (Singularity.Instance.loadedObjects.Count != 0)
+            {
+                GUILayout.BeginVertical();
+                GUILayout.Label("Choose singularity to edit");
+                selGridInt = GUILayout.SelectionGrid(selGridInt, selStrings, 1);
+                if (GUILayout.Button("Edit Selected"))
+                {
+                    if (!ReferenceEquals(Singularity.Instance.loadedObjects[selGridInt], null))
+                    {
+                        nodeText = string.Copy(Utils.WriteRootNode(ConfigNode.CreateConfigFromObject(Singularity.Instance.loadedObjects[selGridInt])));
+                        editing = true;
+                    }
+                }
+                GUILayout.EndVertical();
+
+                if (editing)
+                {
+                    scrollPosition = GUILayout.BeginScrollView(scrollPosition, false, true, GUILayout.MinHeight(300));
+                    nodeText = GUILayout.TextArea(nodeText, GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true));
+                    GUILayout.EndScrollView();
+
+                    GUILayout.BeginHorizontal();
+
+                    if (GUILayout.Button("Reimport"))
+                    {
+                        nodeText = string.Copy(Utils.WriteRootNode(ConfigNode.CreateConfigFromObject(Singularity.Instance.loadedObjects[selGridInt])));
+                    }
+
+                    if (GUILayout.Button("Apply"))
+                    {
+                        ConfigNode node = ConfigNode.Parse(nodeText);
+                        Singularity.Instance.loadedObjects[selGridInt].ApplyFromUI(node);
+                    }
+
+                    if (GUILayout.Button("Copy to clipboard"))
+                    {
+                        GUIUtility.systemCopyBuffer = nodeText;
+                    }
+
+                    if (GUILayout.Button("Print to Log"))
+                    {
+                        Utils.LogInfo("Config print to log:\r\n" + nodeText);
+                    }
+
+                    GUILayout.EndHorizontal();
+                }
+            }
+            else
+            {
+                GUILayout.Label("No Singularity objects loaded");
+            }
+
+            GUI.DragWindow();
+        }
+    }
 }
 
 ;
